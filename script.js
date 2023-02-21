a97f8f4cdf26561e4254fcaaff9a42abcc46eab6
levidel <123884591+levidel@users.noreply.github.com>
2023-02-21 19:37:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+/*const overflowbutton1 = document.getElementById("overflow1")
+const overflowtext = documt.getElementById("overflowtext")
+let overflowhidden = document.getElementsByClassName("overflowhidden")
+let overflowshown = document.getElementsByClassName("overflowshown")
+
+overflowbutton1.addEventListener('click', onclick)
+
+function onclick(e) {
+
+if (
+
+}
+
+function 
+*/
+/*
+const toggleButton = document.getElementById('#overflowButton1');
+toggleButton.addEventListener('click', toggleClass);
+
+
+
+function toggleClass(){
+  const element = document.querySelector('.overflowhidden');
+  element.classList.toggle('overflowshown');
+}
+
+
+console.log(element)
+
+
+
+const toggleButton = document.getElementById('#uil');
+
+function alerttest() {
+
+  alert('testboodschap')
+}
+
+toggleButton.addEventListener("click", alerttest)
+
+*/
+/*
+const hoverknop = document.getElementById('#testbutton');
+
+console.log(hoverknop)
+
+function hovereffect() {
+  alert('hovereffect')
+
+};
+
+hoverknop.addEventListener("mouseover", hovereffect);
+*/
+
